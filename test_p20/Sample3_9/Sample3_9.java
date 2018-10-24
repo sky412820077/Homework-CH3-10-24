b720f3404c5574900c4b531fb16557c5a1558f2d
sky412820077 <sky412820077@gmail.com>
2018-10-24 20:37:26 +0800

    hw3

---
+++ test_p20/Sample3_9/Sample3_9.java
@@ -0,0 +1,28 @@
+package Sample3_9;
+public class Sample3_9 
+{
+	public static void main(String[] args)
+	{
+		Car car1;
+		car1 = new Car();
+
+		car1.num = 1234;
+		car1.gas = -10;
+		
+		car1.show();
+	}
+}
+
+//Car類別
+class Car
+{
+	int num;
+	double gas;
+
+	void show()
+	{
+		System.out.println("車號是"+this.num);
+		System.out.print("汽油量是"+this.gas);
+	}
+}
+
---
+++ test_p5/Sample3_1/Sample3_1.java
@@ -0,0 +1,14 @@
+package Sample3_1;
+public class Sample3_1
+{
+	public static void main(String[] args)
+	{
+		Car car1;
+		car1 = new Car();
+	}
+}
+
+class Car
+{
+	//CarÃş§O
+}
